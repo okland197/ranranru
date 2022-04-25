d995c77cbb5250aaf8014dd0b7a6ec94e3574be3
O_O <gomtang5555@gmail.com>
2022-04-25 15:20:23 +0900

    -- 약간의 정리정돈

--- GAME/game.py
+++ GAME/game.py
@@ -3,75 +3,110 @@ import random
 
 pygame.init()
 
+##게임 기본 설정ㄱ-----------------------------------------------------------------------
+
+#프레임 설정을 위한 시간
+clock = pygame.time.Clock()
+#화면 크기 조절
+screen_width = 640
+screen_height = 512
+#디스플레이 크기 설정
+screen = pygame.display.set_mode((screen_width, screen_height))
+#원도우 창 위 제목설정
+pygame.display.set_caption("테스트")
+#현재 프레임 값
+fps = 1
+
+##소리 설정ㄱ---------------------------------------------------------------------------
+
+#소리 꺼내오기
 background_Sound = pygame.mixer.Sound("Background.wav")
 effect_gun_Sound = pygame.mixer.Sound("Gun.wav")
 Redzone_war_Sound = pygame.mixer.Sound("Redzone_war.wav")
+
+#소리 설정하기
 effect_gun_Sound.set_volume(0.5)
 background_Sound.play(-1)
 
+##이미지 불러오기ㄱ----------------------------------------------------------------------
 
-
-screen_width = 640
-screen_height = 512
-
-screen = pygame.display.set_mode((screen_width, screen_height)) #디스플레이 설정
-
-pygame.display.set_caption("테스트") #게임 제목
-
-background = pygame.image.load("background.png") #이미지 로드
-
-char_sprite = [pygame.image.load("char1.png"),pygame.image.load("char2.png")]
+background = pygame.image.load("background.png")
 char_attack_sprite = [pygame.image.load("char_Attack1.png"),pygame.image.load("char_Attack2.png")]
 wall_sprite = [pygame.image.load("wall1.png")]
 Redzone_w_sprite = [pygame.image.load("redzone-warning.png")]
 boom_sprite = [pygame.image.load("boom1.png"),pygame.image.load("boom2.png"),pygame.image.load("boom3.png")]
 
-clock = pygame.time.Clock()
+##(주인공)캐릭터 기본 설정 및 슈팅 설정ㄱ---------------------------------------------------
 
+##(주인공)캐릭터ㄱ
+#(주인공)캐릭터 이미지 불러오기
+char_sprite = [pygame.image.load("char1.png"),pygame.image.load("char2.png")]
+#(주인공)캐릭터 이미지 크기 값 불러오기
 a = char_sprite[0].get_size()
-
+#(주인공)캐릭터가 최초로 있을 위치 계산
 charX = (screen_width/2)-(a[0]/2)
 charY = (screen_height/2)-(a[1]/2)
-
-move_X = 0 #움직이는 방향
-move_Y = 0 #움직이는 방향
-check = [0,0,0,0,0] #누르고 있는 중
-
+#(주인공)캐릭터가 움직이는 속도
 Vspeed = 4
+#(주인공)캐릭터가 X방향으로 움직이는 변수
+move_X = 0
+#(주인공)캐릭터가 Y방향으로 움직이는 변수
+move_Y = 0
+#특정키 누르는 중이란 것을 표현하기 위한 변수
+#왼쪽,오른쪽,아래,위,스페이스
+check = [0,0,0,0,0]
+#(주인공)캐릭터 애니메이션 프레임 값
 chr_s_num = 0
-fps = 1
-
-shot_list = []
-del_shot_list = []
-shotFPS = 0
-
-#올라가는 벽
-wall_FPS = 0
-wall_list = []
-
-#레드존 공격
-RedZone_FPS = 0
-RedZone_list = []
-redzone_war = False
-redzone_Boom = False
-attackX,attackY = [0,0]
-
-
-
-
-
+#애니메이션 프레임 값 0 1을 내보내는 값
 animation_FP = 0
 
+##슈팅ㄱ
+#화면에서 현재 슈팅이 띄어지는 위치들 목록
+shot_list = []
+#슈팅이 특정이벤트에 의해 지워질 리스트들
+del_shot_list = []
+#슈팅 애니메이션 프레임 값 (근데 움직이는게 안 보여)
+shotFPS = 0
+
+##방해물 설정ㄱ---------------------------------------------------------------------------
+
+#벽 움직이는 속도를 맟추기 위한 변수
+wall_FPS = 0
+#현재 화면에 있는 벽들 리스트
+wall_list = []
+
+##어려운 방해물들 설정ㄱ--------------------------------------------------------------------
+
+##레드존 공격ㄱ
+#현재 화면에 몇프레임까지 띄울 것 인지를 표현하기 위한 변수
+RedZone_FPS = 0
+#현재 화면에 존재하는 레드존 리스트들
+RedZone_list = []
+#레드존 경고 시작
+redzone_war = False
+#레드존 폭팔 시작
+redzone_Boom = False
+#레드존 랜덤 위치를 저장하기 위한 변수
+attackX,attackY = [0,0]
+
+#---------------------------------------------------------------------------------------
+
+
+
+##함수 설정ㄱ-----------------------------------------------------------------------------
+
+##(주인공)캐릭터 애니메이션이 움직이는 함수ㄱ
 def anime(A):
     for i in range(10):
         if i%2 == 0 or i == 0:
-            if (A*i) < fps < (A*(i+1)):  # 주인공 애니메이션
+            if (A*i) < fps < (A*(i+1)):
                 animation_FP = 0
                 return animation_FP
     else:
         animation_FP = 1
         return animation_FP
 
+##슈팅 함수ㄱ
 def shooting(A):
     global shotFPS
 
@@ -106,7 +141,7 @@ def shooting(A):
                 except:
                     return
 
-
+##방해물 함수ㄱ
 def wall_move(A,B,C):
     global wall_FPS
 
@@ -139,6 +174,7 @@ def wall_move(A,B,C):
             if wall_list[i][1] < -64:
                 del wall_list[i]
 
+##레드존 함수ㄱ
 def RedZone(A,B):
     global RedZone_FPS
     global redzone_war
@@ -191,6 +227,7 @@ def RedZone(A,B):
             redzone_Boom = False
             RedZone_FPS = 0
 
+##(주인공)캐릭터랑 충돌하는 함수ㄱ
 def collision_check(A,B,C):
     for i in range(len(C)):
         if B+5 < C[i][1]+64 and C[i][1] < B+31 and C[i][0] < A+31 and A+5 < C[i][0]+64:
@@ -198,16 +235,17 @@ def collision_check(A,B,C):
     else:
         return False
 
-running = True #게임 진행 여부
-
+#게임 진행 여부----------------------------------------------------------------------------
+running = True
+#게임 진행--------------------------------------------------------------------------------
 while running:
     dt = clock.tick(120)
     animation_FP = anime(15)
-    screen.blit(background, (0, 0))  # 배경 그리기
+    screen.blit(background, (0, 0))
 
-    for event in pygame.event.get(): #pygame 이벤트 속성을 가져온다
-        if event.type == pygame.QUIT: #창끄는 버튼입력을 받았을 경우
-            running = False #진행 끄기s
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_KP_4:
                 check[0] = 1
@@ -274,7 +312,6 @@ while running:
 
     Chracter = screen.blit(char_sprite[animation_FP], (charX, charY))
 
-
     shooting(5)
 
     wall_move(100, 2, 3)
@@ -284,7 +321,6 @@ while running:
     else:
         fps += 1
 
-    pygame.display.update() #업데이트 하기
-
+    pygame.display.update()
 
 pygame.quit()
